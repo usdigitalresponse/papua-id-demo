2528053717dd342ce8b54b5a3f93874ea7cc89dd
Matthew Schultz <abstractapproach@gmail.com>
2020-06-11 17:12:06 -0500

    fix user-circle

--- app/helpers/descision_helper.rb
+++ app/helpers/descision_helper.rb
@@ -9,7 +9,7 @@ module DescisionHelper
       icon_class = 'fas fa-times-circle'
     when 'Manual Review'
       text_class = 'text-warning'
-      icon_class = 'fa-user-circle'
+      icon_class = 'fas fa-user-circle'
     end
     icon_class << " fa-#{options[:size]}x" if options[:size].present?
     content_tag(:span, content_tag(:i, '', class: icon_class), class: text_class)
