bb55d9c09eef5570931ecd0ec7e5e12332e7dfdc
Matthew Schultz <abstractapproach@gmail.com>
2020-06-11 17:08:13 -0500

    Begin of admin page

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -1,5 +1,17 @@
 class Admin::ApplicantsController < Admin::AdminController
+  before_action :set_applicant, only: [:show]
+
   def index
     @applicants = Applicant.for_current_workflow
+  end
+
+  def show
+    
+  end
+
+  protected
+
+  def set_applicant
+    @applicant = Applicant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'root#index'
   namespace :admin do
-    resources :applicants
+    resources :applicants, only: [:index, :show]
   end
   resources :applicants, only: [:new, :create, :show]
 end
