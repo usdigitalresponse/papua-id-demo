5fae844c0f38de913406924f2a65c6d3e581191e
Matthew Schultz <abstractapproach@gmail.com>
2020-06-29 09:48:59 -0500

    Redirect to login screen for login-required pages if not logged in

--- app/constraints/admin_constraint.rb
+++ app/constraints/admin_constraint.rb
@@ -1,5 +1,6 @@
 class AdminConstraint
   def matches?(request)
+    request.define_singleton_method(:application_requires_login_in_admin_constraint) { true }
     return false unless request.session[:user_id]
     User.find(request.session[:user_id]).present?
   end
