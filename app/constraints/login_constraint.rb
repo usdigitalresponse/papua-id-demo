6bab3e2acb09641d1f13a20a764c1e495958bf0a
Matthew Schultz <abstractapproach@gmail.com>
2020-06-30 07:49:12 -0500

    fix

--- app/constraints/login_constraint.rb
+++ app/constraints/login_constraint.rb
@@ -1,5 +1,5 @@
 class LoginConstraint
   def matches?(request)
-    request.path.exclude? 'rails/active_storage' && request&.application_requires_login_in_admin_constraint
+    request.path.exclude? 'rails/active_storage' # && request&.application_requires_login_in_admin_constraint
   end
 end
