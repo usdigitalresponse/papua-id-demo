5ab320afdf940b7dcc77f57a8fc1d91a5dc6d01c
Matthew Schultz <abstractapproach@gmail.com>
2020-07-06 12:42:47 -0500

    Turn off login

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ require 'sidekiq/web'
 require 'admin_constraint'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq', :constraints => AdminConstraint.new
+  mount Sidekiq::Web => '/sidekiq' #, :constraints => AdminConstraint.new
   root to: 'root#index'
   get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/login', to: 'sessions#new', as: 'login'
   resources :sessions, only: [:new, :create, :destroy]
-  namespace :admin, constraints: AdminConstraint.new do
+  namespace :admin do #, constraints: AdminConstraint.new do
     resources :applicants, only: [:index, :show]
     resources :documents, only: [:show]
   end
