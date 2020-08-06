fd17699161c7f2bf0f92d3e49bcab283212fdf58
Matthew Schultz <abstractapproach@gmail.com>
2020-08-06 15:38:50 -0500

    show all applicants

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -2,7 +2,7 @@ class Admin::ApplicantsController < Admin::AdminController
   before_action :set_applicant, only: [:show, :show2]
 
   def index
-    @applicants = Applicant.for_current_workflow.order(created_at: :desc)
+    @applicants = Applicant.all.order(created_at: :desc)
   end
 
   def show
