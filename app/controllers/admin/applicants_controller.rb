ff67d8f283fbdfa20a65a9c5231e264da9939b8d
Raymond Tang <raymond@tang.me>
2020-08-10 19:47:02 -0400

    handle empty search and no query param

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -2,7 +2,7 @@ class Admin::ApplicantsController < Admin::AdminController
   before_action :set_applicant, only: [:show, :show2]
 
   def index
-    if params["query"]
+    if params["query"] != nil && !params["query"].strip.empty?
       @filter = params["query"]
       @applicants = Applicant.all.order(created_at: :desc).global_search("#{@filter}")
     else
