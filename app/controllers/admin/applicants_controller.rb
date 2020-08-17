ac65cad752381f887c678ff14cc3bbaa704165c5
Matthew Schultz <abstractapproach@gmail.com>
2020-08-17 11:39:06 -0500

    Update search to run on show action

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -1,13 +1,11 @@
 class Admin::ApplicantsController < Admin::AdminController
   before_action :set_applicant, only: [:show, :show2]
+  before_action :search, only: [:index, :show]
 
   def index
-    @applicants = Applicant.all.order(created_at: :desc)
-    @applicants = @applicants.global_search(params["query"]) if params["query"].present?
   end
 
   def show
-
   end
 
   def show2
@@ -17,5 +15,10 @@ class Admin::ApplicantsController < Admin::AdminController
 
   def set_applicant
     @applicant = Applicant.find(params[:id])
+  end
+
+  def search
+    @applicants = Applicant.all.order(created_at: :desc)
+    @applicants = @applicants.global_search(params["query"]) if params["query"].present?
   end
 end
