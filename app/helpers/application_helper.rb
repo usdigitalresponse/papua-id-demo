ea0322a0595468f6889faf994be6e5fa3cf1622d
Mehal Shah <mehal.shah+github@gmail.com>
2020-08-19 15:56:41 -0700

    Checkpoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  @@personal_line_items = [:first_name, :last_name]
+
+
+  def applicant_line_item_decision_icon(applicant, line_item)
+    if @@personal_line_items.any? line_item
+      case applicant.line_item_decisions.find { |x| x.name == line_item.to_s}.decision.to_sym
+      when :approved
+        :check_circle
+      when :rejected
+        :times_circle
+      when :pending
+        :question_circle
+      default
+        :circle
+      end
+    end
+  end
 end
--- app/models/line_item_decision.rb
+++ app/models/line_item_decision.rb
@@ -1,5 +1,3 @@
 class LineItemDecision < ApplicationRecord
   belongs_to :decidable, polymorphic: true
-
-  enum decision: [ :approved, :manual_review, :rejected]
 end
