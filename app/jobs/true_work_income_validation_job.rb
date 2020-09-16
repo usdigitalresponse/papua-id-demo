725b172027bc80dfa904801b80849afb50e8c2a0
smedberg <smedberg@gmail.com>
2020-09-16 10:52:32 -0700

    Moving TrueWork ID to "output" hash

--- app/jobs/true_work_income_validation_job.rb
+++ app/jobs/true_work_income_validation_job.rb
@@ -1,12 +1,12 @@
 class TrueWorkIncomeValidationJob < ApplicationJob
   queue_as :default
 
-  def perform(validation_id, addl_info = {})
+  def perform(validation_id)
     validation = TrueWorkIncomeValidation.find(validation_id)
     if validation.status == "started"
         validation.initiate_validation
     elsif validation.status == "in_process"
-        validation.poll_validation(addl_info)
+        validation.poll_validation
     else
         raise "Error: TrueWorkIncomeValidationJob.perform called on validation in state #{validation.status}"
     end
--- app/models/true_work_income_validation.rb
+++ app/models/true_work_income_validation.rb
@@ -48,9 +48,10 @@ class TrueWorkIncomeValidation < IncomeValidation
         if verification_request[:turnaround_time]
           check_back_hours = verification_request[:turnaround_time][:lower_bound]
         end
+        output = {verification_id: verification_request[:id]}
+        update_attributes(output: output)
 
-        verification_info = {verification_id: verification_request[:id]}
-        job_class.set(wait: check_back_hours.hours).perform_later(id, verification_info)
+        job_class.set(wait: check_back_hours.hours).perform_later(id)
       end
     rescue Truework::ClientException, Truework::ServerException => e
         output = {
@@ -60,39 +61,39 @@ class TrueWorkIncomeValidation < IncomeValidation
     end
   end
 
-  def poll_validation(verification_info)
+  def poll_validation
     begin
-      verification_request = Truework::VerificationRequest.retrieve(verification_info[:verification_id])
+      verification_request = Truework::VerificationRequest.retrieve(output[:verification_id])
       case verification_request[:state]
       when "completed"
         report = Truework::Report.retrieve(verification_info[:verification_id])
-        output = {
+        new_output = output.merge({
           employment_status: report[:status], # active, inactive, or unknown
           salary: report[:gross_pay],
           salary_frequency: report[:pay_frequency], # This is a string; not clear what allowed values are 
           employment_type: report[:employment_type], # regular-full-time, regular-part-time, contractor, or other
           earnings: report[:earnings] # There's potentially a lot of info here; do we want all of it?
-        }
-        update_attributes(status: :complete, output: output)
+        })
+        update_attributes(status: :complete, output: new_output)
         # TODO: How does "self.decision" get set?
       when "canceled"
-        output = {
+        new_output = output.merge({
           error_info: verification_request[:cancellation_reason],
           error_details: verification_request[:cancellation_details]
-        }
-        update_attributes(status: :error, output: output)
+        })
+        update_attributes(status: :error, output: new_output)
       when "invalid"
         # TODO: Can TrueWork tell us why our request is invalid?
-        output = {
+        new_output = output.merge({
           error_info: "TrueWork indicates that request made by this app is invalid; this is probably a bug.  Further info is not available at this time."
-        }
-        update_attributes(status: :error, output: output)
+        })
+        update_attributes(status: :error, output: new_output)
       when "action-required"
         # TODO: How do we handle this case?
-        output = {
+        new_output = output.merge({
           error_info: "TrueWork requires more information to process request, but we do not have a way to supply that information."
-        }
-        update_attributes(status: :error, output: output)
+        })
+        update_attributes(status: :error, output: new_output)
       else
         # Processing is not complete, poll again later
         check_back_hours = 1
@@ -104,10 +105,10 @@ class TrueWorkIncomeValidation < IncomeValidation
         job_class.set(wait: check_back_hours.hours).perform_later(id, verification_info)
       end
     rescue Truework::ClientException, Truework::ServerException => e
-        output = {
+        new_output = output.merge({
           error_info: e.to_s
-        }
-        update_attributes(status: :error, output: output)
+        })
+        update_attributes(status: :error, output: new_output)
     end
   end
 end
